ba1935953b9997ca65d948f8f90a632849b22672
dvitale199 <d.vitale199@gmail.com>
2021-06-22 02:37:01 -0400

    testing impuation pipeline run from swarm

--- QC/imputation.py
+++ QC/imputation.py
@@ -7,16 +7,22 @@ import time
 import glob
 
 # local imports
-from QC.utils import shell_do, rm_tmps, count_file_lines
+from QC.utils import shell_do
 
-def impute_prep_data(geno_path, out_path):
+def impute_data_prep(geno_path, out_path, ref_panel, check_bim_pl):
+
+    '''
+    info here:
+    https://www.well.ox.ac.uk/~wrayner/tools/
+    '''
     
-    
+
+    out_dir = out_path.rsplit('/', 1)[0]
     workdir = os.getcwd()
-    os.chdir(out_path)
+    os.chdir(out_dir)
 
-    plink1 = f'plink --bfile {geno_path} --freq --out {geno_path}'
-    check_bim_cmd = f'perl {ref_path}/HRC-1000G-check-bim.pl -b {geno_path}.bim -f {geno_path}.frq -r {ref_path}/HRC.r1-1.GRCh37.wgs.mac5.sites.tab -h'
+    plink1 = f'plink --bfile {geno_path} --freq --out {out_path}'
+    check_bim_cmd = f'perl {check_bim_pl} -b {geno_path}.bim -f {out_path}.frq -r {ref_panel} -h'
     bash1 = 'sh Run-plink.sh'
 
     cmds = [plink1, check_bim_cmd, bash1]
@@ -25,3 +31,136 @@ def impute_prep_data(geno_path, out_path):
         shell_do(cmd)
 
     os.chdir(workdir)
+    
+    mk_vcf_cmds = [f'plink --bfile {out_path}-updated-chr{str(i)} --recode vcf --chr {str(i)} --out {out_path}_chr{str(i)}' for i in range(1,24)]   
+
+    for cmd in mk_vcf_cmds:
+        shell_do(cmd)
+
+    # ## then sort and zip
+    sort_zip_cmds = [f'vcf-sort {out_path}_chr{str(i)}.vcf | bgzip -c > {out_path}_pre_impute_chr{str(i)}.vcf.gz' for i in range(1,24)]
+
+    for cmd in sort_zip_cmds:
+        subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
+    
+    vcf_outpaths = [f'{out_path}_pre_impute_chr{str(i)}.vcf.gz' for i in range(1,24)]
+    return {'vcfs': vcf_outpaths}
+
+
+def check_impute_status(token, job_id):
+        
+    # imputation server url
+    url = 'https://imputation.biodatacatalyst.nhlbi.nih.gov/api/v2'
+
+    # add token to header (see authentication)
+    headers = {'X-Auth-Token' : token }
+
+    # get all jobs
+    r = requests.get(url + "/jobs", headers=headers)
+    if r.status_code != 200:
+        raise Exception('GET /jobs/ {}'.format(r.status_code))
+
+    status = r.json()
+    for stat in status['data']:
+        if stat['id'] == job_id:
+            if stat['state'] == 1:
+                print("Launching Job:", stat['id'])
+            elif stat['state'] == 2:
+                print("Running Job:", stat['id'])
+            elif stat['state'] == 3:
+                print(stat['id'], "returned state '3', have a look at jobs on the web front for more information")
+            elif stat['state'] == 5:
+                print(stat['id'], "has failed. consult docs on data input to ensure your vcfs are correct")
+            elif stat['state'] == 4:
+                print(stat['id'], "COMPLETED!")
+
+            return stat['state']
+
+        else:
+            pass
+
+        
+def pull_imputed_data(out_path, token, job_id, password):
+    
+    workdir = os.getcwd()
+    os.chdir(out_path)
+    
+    # imputation server url
+    url = 'https://imputation.biodatacatalyst.nhlbi.nih.gov/api/v2'
+
+    # add token to header (see authentication)
+    headers = {'X-Auth-Token' : token }
+
+    r = requests.get(f'{url}/jobs/{job_id}', headers=headers)
+    if r.status_code != 200:
+        raise Exception(f'GET /jobs/ {r.status_code}')
+
+    output_json = r.json()
+
+    hashes_dict = {output_json['outputParams'][i]['id'] : output_json['outputParams'][i]['hash'] for i in range(len(output_json['outputParams']))}
+
+    # run a curl for each
+    curls = [f'curl -sL https://imputation.biodatacatalyst.nhlbi.nih.gov/get/{str(key)}/{str(hashes_dict[key])} | bash' for key in hashes_dict]
+    
+    for curl in curls:
+        print(f"Curling output data with the following command: {curl}")
+        subprocess.run(curl, shell=True)
+    print() 
+    print("Finished Pulling Imputed Data!")
+    print()
+    
+    os.chdir(workdir)
+    
+
+def submit_job(vcf_list,
+               password, token=None):
+    # test topmed server
+    url = 'https://imputation.biodatacatalyst.nhlbi.nih.gov/api/v2'
+    
+    # add token to header (see Authentication)
+    headers = {'X-Auth-Token' : token}
+
+    open_vcfs = [open(vcf, 'rb') for vcf in vcf_list]
+
+    files = set([('input-files-upload', vcf) for vcf in open_vcfs])
+    # files = {'input-files': open(vcf_list[0], 'rb')}
+
+    data = {'input-mode' : 'imputation',
+            'input-files-source': 'file-upload',
+            'input-password': password,
+            'input-refpanel': 'apps@topmed-r2@1.0.0',
+            'input-phasing': 'eagle',
+            'input-population': 'all'}
+
+    r = requests.post(url + "/jobs/submit/imputationserver", files=files, headers=headers, data=data)
+    if r.status_code != 200:
+        raise Exception('POST /jobs/submit/imputationserver {}'.format(r.status_code))
+
+    job_id = r.json()['id']
+    message = r.json()['message']
+    print(job_id, message)
+    
+    print('***************************')
+    print('* * * * * * * * * * * * * *')
+    
+    return r.json()
+
+
+def run_auto_imputation(geno_path, temp_path, out_path, token,  ref_panel, check_bim_pl, password='imputer'):
+    
+    impute_data = impute_data_prep(geno_path, temp_path, ref_panel, check_bim_pl)
+    
+    job_json = submit_job(impute_data['vcfs'], password=password, token=token)
+    job_id = job_json['id']
+    
+    imp_state = 0
+    while imp_state < 3:
+        time.sleep(600)
+        os.system('clear')
+        imp_state = check_impute_status(token, job_id)
+
+        if imp_state == 4:
+            print("Pulling Completed Data from Imputation Server!")
+            pull_imputed_data(out_path=out_path, token=token, job_id=job_id, password=password)
+    
+    return job_json
--- run_qc_pipeline.py
+++ run_qc_pipeline.py
@@ -12,8 +12,7 @@ parser = argparse.ArgumentParser(description='Arguments for Genotyping QC (data 
 parser.add_argument('--geno', type=str, default='nope', help='Genotype: (string file path). Path to PLINK format genotype file, everything before the *.bed/bim/fam [default: nope].')
 parser.add_argument('--ref', type=str, default='nope', help='Genotype: (string file path). Path to PLINK format reference genotype file, everything before the *.bed/bim/fam.')
 parser.add_argument('--ref_labels', type=str, default='nope', help='tab-separated plink-style IDs with ancestry label (FID  IID label) with no header')
-parser.add_argument('--out', type=str, default='out', help='Prefix for output (including path)')
-# parser.add_argument('--rare', default=False, action="store_true", help='Pruning toggle for rare variants. If --rare is used, final MAF pruning (0.01) will not be conducted, otherwise, rare variants will be pruned')
+parser.add_argument('--out', type=str, default='nope', help='Prefix for output (including path)')
 
 args = parser.parse_args()
 
@@ -145,107 +144,3 @@ total_umap.to_hdf(metrics_outfile, key='total_umap')
 ref_umap.to_hdf(metrics_outfile, key='ref_umap')
 new_samples_umap.to_hdf(metrics_outfile, key='new_samples_umap')
 
-
-
-
-
-
-
-# geno_path = '/data/CARD/PD/GP2/raw_genotypes/shulman_ny/plink/shulman'
-# hard code in reference for now--- definitely change later
-# ref_dir_path = '/data/LNG/vitaled2/1kgenomes'
-# ref_panel = f'{ref_dir_path}/1kg_ashkj_ref_panel_gp2_pruned'
-# ref_labels = f'{ref_dir_path}/ref_panel_ancestry.txt'
-
-# steps = []
-
-# avg_miss = avg_miss_rates(geno_path, f'{geno_path}_missing')
-
-# # prune callrate
-# callrate_out = f'{geno_path}_callrate'
-# callrate = callrate_prune(geno_path, callrate_out)
-# steps.append(callrate)
-
-# # prune sex
-# sex_out = f'{callrate_out}_sex'
-# sex = sex_prune(callrate_out, sex_out)
-# steps.append(sex)
-
-# # ancestry estimation
-# ancestry_out = f'{sex_out}_ancestry'
-# ancestry = run_ancestry(geno_path=sex_out, out_path=ancestry_out, ref_panel=ref_panel, ref_labels=ref_labels)
-
-# # split cohort by ancestry group
-# pred_labels_path = ancestry['output']['predicted_labels']['labels_outpath']
-# cohort_split = split_cohort_ancestry(geno_path=sex_out, labels_path=pred_labels_path, out_path=ancestry_out)
-
-# # run 
-# het_dict = dict()
-# related_dict = dict()
-# variant_dict = dict()
-# avg_miss_dict = dict()
-
-# for geno, label in zip(cohort_split['paths'], cohort_split['labels']):
-
-#     # het
-#     het_out = f'{geno}_het'
-#     het = het_prune(geno, het_out)
-#     het_dict[label] = het
-#     # steps.append(het)
-
-#     # related
-#     related_out = f'{het_out}_related'
-#     related = related_prune(het_out, related_out)
-#     related_dict[label] = related
-#     # steps.append(related)
-#     # variant
-#     variant_out = f'{related_out}_variant'
-#     variant = variant_prune(related_out, variant_out)
-#     variant_dict[label] = variant
-#     # steps.append(variant)
-#     avg_miss_out = f'{variant_out}_missing'
-#     avg_miss = avg_miss_rates(variant_out, avg_miss_out)
-#     avg_miss_dict[label] = avg_miss
-
-#     steps2 = [het_dict, related_dict, variant_dict]
-
-# with open(f'{out}_QC_REPORT.txt', 'w') as f:
-#     f.write("QC REPORT\n")
-#     f.write("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n")
-#     f.write("$$$$$$$$$$$$$$ Whole cohort steps $$$$$$$$$$$$$$$\n")
-#     f.write("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n")
-#     f.write("\n")
-#     f.write(f"Missingness per Ancestry BEFORE QC: {avg_miss}\n")
-#     f.write("\n")
-
-#     for step in steps:
-#         f.write("\n")
-#         f.write(f"STEP: {step['step']}\n")
-#         f.write(f"Metrics: {step['metrics']}\n")
-#         f.write("\n")
-    
-#     f.write("\n")
-#     f.write("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n")
-#     f.write("$$$$$$$$$$ Individual Ancestry steps $$$$$$$$$$$$\n")
-#     f.write("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n")
-#     f.write("\n")
-    
-#     for item in steps2:
-#         for key, step in item.items():
-#             f.write("\n")
-#             f.write(f"Ancestry Group: {key}\n")
-#             f.write(f"STEP: {step['step']}\n")
-#             f.write(f"{step['metrics']}\n")
-#             f.write("\n")
-
-#     for key, value in avg_miss_dict.items():
-#         f.write("\n")
-#         f.write(f"Ancestry Group: {key}\n")
-#         f.write(f"Missingness per Ancestry AFTER QC: {value}\n")
-#         f.write("\n")
-    
-#     f.write("\n")
-#     f.write("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n")
-#     f.write("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n")
-#     f.write("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n")
-#     f.write("\n")
