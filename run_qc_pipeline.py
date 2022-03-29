398c7f42db8c8f9850acd4ed8149907ef33f8ab9
Dan Vitale <d.vitale199@gmail.com>
2022-03-29 11:38:12 -0400

    Update run_qc_pipeline.py

--- run_qc_pipeline.py
+++ run_qc_pipeline.py
@@ -14,7 +14,7 @@ parser.add_argument('--geno', type=str, default='nope', help='Genotype: (string 
 parser.add_argument('--ref', type=str, default='nope', help='Genotype: (string file path). Path to PLINK format reference genotype file, everything before the *.bed/bim/fam.')
 parser.add_argument('--ref_labels', type=str, default='nope', help='tab-separated plink-style IDs with ancestry label (FID  IID label) with no header')
 parser.add_argument('--model', type=str, default=None, help='Path to pickle file with trained ancestry model for passed reference panel')
-parser.add_argument('--callrate', type=str, default=None, help='Minimum Callrate threshold for QC')
+parser.add_argument('--callrate', type=float, default=0.02, help='Minimum Callrate threshold for QC')
 parser.add_argument('--out', type=str, default='nope', help='Prefix for output (including path)')
 
 args = parser.parse_args()
