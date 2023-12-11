fad5592a37de4344a243cffb171466adc7abf989
dvitale199 <d.vitale199@gmail.com>
2023-12-11 15:39:55 -0500

    added license to all files

--- genotools/dependencies.py
+++ genotools/dependencies.py
@@ -1,6 +1,4 @@
-# From: https://github.com/GenoML/genoml2
-#
-# Copyright 2020 The GenoML Authors. All Rights Reserved.
+# Copyright 2023 The GenoTools Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
 
 import io
 import logging
