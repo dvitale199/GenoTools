c8fa605185898d56a8572a6e984c5d873f0615ff
mkoretsky1 <mathew.koretsky1@gmail.com>
2024-01-08 13:50:31 -0500

    when available, add ancestry labels to pruned samples file

--- genotools/__main__.py
+++ genotools/__main__.py
@@ -194,8 +194,12 @@ def handle_main():
     for df in [metrics_df, pruned_df, gwas_df]:
         if not df.empty:
             clean_out_dict['QC'] = metrics_df.to_dict()
-            clean_out_dict['pruned_samples'] = pruned_df.to_dict()
             clean_out_dict['GWAS'] = gwas_df.to_dict()
+
+            if 'ancestry_labels' in list(clean_out_dict.keys()):
+                labels = pd.DataFrame(clean_out_dict['ancestry_labels'])
+                pruned_df = pruned_df.merge(labels[['IID','label']], how='left', on=['IID'])
+                clean_out_dict['pruned_samples'] = pruned_df.to_dict()
 
     # dump output to json
     with open(f'{args_dict["out"]}.json', 'w') as f:
