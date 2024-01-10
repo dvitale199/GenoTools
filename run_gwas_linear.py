03e230fba181f87b8629bfd5ba3f3ed2b1c85ad4
mikayla-james <mlouisejames11@gmail.com>
2024-01-10 18:28:15 -0500

    minor

--- run_gwas_linear.py
+++ run_gwas_linear.py
@@ -4,6 +4,7 @@ import os
 
 # local imports
 from genotools.gwas import *
+from genotools.gwas import run_plink_pca, run_gwas
 from genotools.utils import *
 
 project_path = '/data/GP2/projects/20240109_AOO_GWAS_/gp2_release6_subset_1k'
