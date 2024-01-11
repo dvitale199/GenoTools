c83534ec95353fbc6e63175ea5ad391a0fca44fa
mikayla-james <mlouisejames11@gmail.com>
2024-01-11 10:55:03 -0500

    troubleshooting

--- run_gwas_linear.py
+++ run_gwas_linear.py
@@ -1,11 +1,14 @@
-import pandas as pd
-import argparse
 import os
-
-# local imports
+print(os.getcwd())
+print(sys.path)
+import pandas as pd
+import numpy as np
+import warnings
+from scipy.stats import ncx2
+from genotools.dependencies import check_plink, check_plink2
 from genotools.gwas import *
-from genotools.gwas import run_plink_pca, run_gwas
 from genotools.utils import *
+
 
 project_path = '/data/GP2/projects/20240109_AOO_GWAS_/gp2_release6_subset_1k'
 pca = run_plink_pca(project_path)
