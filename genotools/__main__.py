3445108b1b5e2121ee40002e7c5f3e72269e66ee
Mathew Koretsky <mathew.koretsky1@gmail.com>
2024-10-07 19:16:13 -0400

    reset __main__

--- genotools/__main__.py
+++ genotools/__main__.py
@@ -164,12 +164,11 @@ def handle_main():
                 geno_path = f'{args_dict["geno_path"]}_ancestry_{label}'
                 out = f'{args_dict["out"]}_{label}'
 
-                # # set new het values if AMR  het is being run
-                # if args_dict['amr_het'] and (label =='AMR'):
-                #     args_dict['het'] = [-1.0,-1.0]
-                # else:
-                #     args_dict['het'] = het_prune_value
-                args_dict['het'] = [-1.0,-1.0]
+                # set new het values if AMR  het is being run
+                if args_dict['amr_het'] and (label =='AMR'):
+                    args_dict['het'] = [-1.0,-1.0]
+                else:
+                    args_dict['het'] = het_prune_value
 
                 out_dict[label] = execute_pipeline(run_steps_list, ordered_steps, geno_path, out, samp_qc=samp_qc, var_qc=var_qc, assoc=assoc, args=args_dict, tmp_dir=tmp_dir)
 
