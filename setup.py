9aebbe7f3de0df3c07c6b0ab59361ab7dd401914
dvitale199 <d.vitale199@gmail.com>
2024-11-15 14:09:13 -0700

    release v1.3.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='the_real_genotools', 
-    version='1.2.4', 
+    version='1.3.0', 
     packages=find_packages(),
     author='Dan Vitale',
     author_email='d.vitale199@gmail.com',
